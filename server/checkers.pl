d0cf5bda354bbb2a221242ca919d13f39caa4365
cryst321 <108535022+cryst321@users.noreply.github.com>
2024-03-24 15:54:03 +0200

    make taking possible backwards and forward, queens can move any amount

--- server/checkers.pl
+++ server/checkers.pl
@@ -322,6 +322,7 @@ next_eat_move(Board,w,X,Y,e(X,Y,X2,Y2,New_Board)):-
 	move(Board,X,Y,X2,Y2,Temp_Board),
 	remove_from_board(Temp_Board,X1,Y1,New_Board).
 
+
 % try to eat to the right
 next_eat_move(Board,w,X,Y,e(X,Y,X2,Y2,New_Board)):-
 	X < 7, Y > 2,
@@ -331,6 +332,31 @@ next_eat_move(Board,w,X,Y,e(X,Y,X2,Y2,New_Board)):-
 	\+is_occupied(Board,X2,Y2),
 	move(Board,X,Y,X2,Y2,Temp_Board),
 	remove_from_board(Temp_Board,X1,Y1,New_Board).
+
+	% try to eat backward left
+    next_eat_move(Board,w,X,Y,e(X,Y,X2,Y2,New_Board)):-
+    	X > 2, Y < 7,
+    	X1 is X - 1, Y1 is Y + 1,
+    	is_enemy(Board,X1,Y1,white),
+    	X2 is X - 2, Y2 is Y + 2,
+    	\+is_occupied(Board,X2,Y2),
+    	move(Board,X,Y,X2,Y2,Temp_Board),
+    	remove_from_board(Temp_Board,X1,Y1,New_Board).
+
+    %try to eat backward right
+    next_eat_move(Board,w,X,Y,e(X,Y,X2,Y2,New_Board)):-
+    	X < 7, Y < 7,
+    	X1 is X + 1, Y1 is Y + 1,
+    	is_enemy(Board,X1,Y1,white),
+    	X2 is X + 2, Y2 is Y + 2,
+    	\+is_occupied(Board,X2,Y2),
+    	move(Board,X,Y,X2,Y2,Temp_Board),
+    	remove_from_board(Temp_Board,X1,Y1,New_Board).
+
+
+
+
+
 
 % try to eat to the left
 next_eat_move(Board,b,X,Y,e(X,Y,X2,Y2,New_Board)):-
@@ -351,6 +377,28 @@ next_eat_move(Board,b,X,Y,e(X,Y,X2,Y2,New_Board)):-
 	\+is_occupied(Board,X2,Y2),
 	move(Board,X,Y,X2,Y2,Temp_Board),
 	remove_from_board(Temp_Board,X1,Y1,New_Board).
+
+
+% try to eat backward left
+next_eat_move(Board,b,X,Y,e(X,Y,X2,Y2,New_Board)):-
+	X > 2, Y > 2,
+	X1 is X - 1, Y1 is Y - 1,
+	is_enemy(Board,X1,Y1,black),
+	X2 is X - 2, Y2 is Y - 2,
+	\+is_occupied(Board,X2,Y2),
+	move(Board,X,Y,X2,Y2,Temp_Board),
+	remove_from_board(Temp_Board,X1,Y1,New_Board).
+
+% try to eat backward right
+next_eat_move(Board,b,X,Y,e(X,Y,X2,Y2,New_Board)):-
+	X < 7, Y > 2,
+	X1 is X + 1, Y1 is Y - 1,
+	is_enemy(Board,X1,Y1,black),
+	X2 is X + 2, Y2 is Y - 2,
+	\+is_occupied(Board,X2,Y2),
+	move(Board,X,Y,X2,Y2,Temp_Board),
+	remove_from_board(Temp_Board,X1,Y1,New_Board).
+
 
 % try to eat forward left
 next_eat_move(Board,wq,X,Y,e(X,Y,X2,Y2,New_Board)):-
@@ -469,15 +517,22 @@ next_move(Board,b,X,Y,m(X,Y,X1,Y1,New_Board)):-
 next_move(Board,Piece,X,Y,m(X,Y,X1,Y1,New_Board)):-
 	queen(Piece),
 	X > 1, Y > 1,
-	X1 is X - 1, Y1 is Y - 1,
+	generate_list(1,7,List),
+	member(N,List),
+	X>N,Y>N,
+	X1 is X - N, Y1 is Y - N,
 	\+is_occupied(Board,X1,Y1),
 	move(Board,X,Y,X1,Y1,New_Board).
+
 
 % move up right
 next_move(Board,Piece,X,Y,m(X,Y,X1,Y1,New_Board)):-
 	queen(Piece),
 	X < 8, Y > 1,
-	X1 is X + 1, Y1 is Y - 1,
+	generate_list(1,7,List),
+	member(N,List),
+	(X + N =<8), Y > N, % Ensure X > N and Y < N
+	X1 is X + N, Y1 is Y - N,
 	\+is_occupied(Board,X1,Y1),
 	move(Board,X,Y,X1,Y1,New_Board).
 
@@ -485,7 +540,10 @@ next_move(Board,Piece,X,Y,m(X,Y,X1,Y1,New_Board)):-
 next_move(Board,Piece,X,Y,m(X,Y,X1,Y1,New_Board)):-
 	queen(Piece),
 	X > 1, Y < 8,
-	X1 is X - 1, Y1 is Y + 1,
+	generate_list(1,7,List),
+	member(N,List),
+	X > N, (Y + N =<8),
+	X1 is X - N, Y1 is Y + N,
 	\+is_occupied(Board,X1,Y1),
 	move(Board,X,Y,X1,Y1,New_Board).
 
@@ -493,9 +551,13 @@ next_move(Board,Piece,X,Y,m(X,Y,X1,Y1,New_Board)):-
 next_move(Board,Piece,X,Y,m(X,Y,X1,Y1,New_Board)):-
 	queen(Piece),
 	X < 8, Y < 8,
-	X1 is X + 1, Y1 is Y + 1,
+	generate_list(1,7,List),
+	member(N,List),
+	(   X+N)=<8,(Y+N)=<8,
+	X1 is X + N, Y1 is Y + N,
 	\+is_occupied(Board,X1,Y1),
 	move(Board,X,Y,X1,Y1,New_Board).
+
 
 queen(wq).
 queen(bq).
